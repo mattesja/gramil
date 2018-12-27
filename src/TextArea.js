8dc6a6db90658b1b38a100414dda01b42d4a7c02
Matt <mattesja@gmail.com>
2018-12-27 14:43:16 +0100

    text

--- src/TextArea.js
+++ src/TextArea.js
@@ -10,22 +10,17 @@ class TextArea extends Component {
 
     state = {
         points: 0,
-        text: "Vor einem a-großen n-Wald v-wohnte ein a-armer n-Holzhacker"
-    };
-
-    /*
-     "Vor einem a-großen n-Wald v-wohnte ein a-armer n-Holzhacker mit seiner n-Frau und seinen a-zwei n-Kindern. Der n-Junge hieß n-Hänsel und das n-Mädchen n-Gretel. " +
+        text: "Vor einem a-großen n-Wald v-wohnte ein a-armer n-Holzhacker mit seiner n-Frau und seinen a-zwei n-Kindern. Der n-Junge hieß n-Hänsel und das n-Mädchen n-Gretel. " +
             "Er v-hatte wenig zu v-beißen und zu v-brechen, und einmal, als eine a-große n-Hungersnot ins n-Land v-kam, v-konnte er auch das a-tägliche n-Brot nicht mehr v-bezahlen. " +
             "Wie er sich nun abends im n-Bett n-Gedanken v-machte und sich vor n-Sorgen v-herumwälzte, v-seufzte er und v-sprach zu seiner n-Frau: " +
             "'Was v-soll nur aus uns v-werden? Wie v-können wir unsere a-armen n-Kinder v-ernähren, da wir für uns selbst nichts mehr v-haben?' " +
-            "'v-Weißt du was?', v-antwortete die n-Frau, 'wir v-wollen morgen früh die n-Kinder hinaus in den n-Wald v-führen, wo er am dicksten ist. " +
+            "' v-Weißt du was?', v-antwortete die n-Frau, 'wir v-wollen morgen früh die n-Kinder hinaus in den n-Wald v-führen, wo er am dicksten ist. " +
             "Da machen wir ihnen ein n-Feuer an und geben jedem noch ein n-Stückchen n-Brot. Dann gehen wir an unsere n-Arbeit und lassen sie allein. " +
             "Sie v-finden den n-Weg nicht wieder nach n-Hause und wir v-sind sie los.' " +
             "'Nein', v-sagte der n-Mann, 'das v-tue ich nicht! Wie v-sollte ich es übers n-Herz v-bringen meine n-Kinder im n-Wald allein zu v-lassen? " +
             "Die a-wilden n-Tiere v-werden bald v-kommen und sie v-zerreißen.' 'O du n-Narr,' sagte sie, 'dann v-müssen wir alle n-Viere am n-Hunger v-sterben. " +
             "Du v-kannst nur die n-Bretter für die n-Särge v-hobeln.' Und sie v-ließ ihm keine n-Ruhe bis er v-einwilligte."
     };
-     */
 
     getWords() {
         const {text} = this.state;
